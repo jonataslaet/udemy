0311d5b94bc5f95257003fe618a4fdac92cc0ccb
jonataslaet <jonataslaetprogramador@gmail.com>
2023-01-29 09:19:41 -0300

    full-stack-angular-spring-boot-tutorial
    Creating loops and arrays

---
+++ ws-full-stack-angular-spring-boot-tutorial/typescrypt-training/03-loops-and-arrays/reviews.ts
@@ -0,0 +1,9 @@
+let myfullname: string[] = ["Jonatas", "Blendo", "dos", "Santos", "Laet"];
+
+for (let i = 0; i < 5; i++) {
+    console.log(myfullname[i]);
+}
+
+for (let name of myfullname) {
+    console.log(name);
+}
