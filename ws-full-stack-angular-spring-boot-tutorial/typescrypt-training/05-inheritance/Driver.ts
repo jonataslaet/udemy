6badc245c582ee983e0f2f3a01e8f327e108752b
jonataslaet <jonataslaetprogramador@gmail.com>
2023-02-02 10:20:19 -0300

    full-stack-angular-spring-boot-tutorial
    Inheritance - Developing a second Subclass
    Inheritance - Adding Shapes to an Array

--- ws-full-stack-angular-spring-boot-tutorial/typescrypt-training/05-inheritance/Driver.ts
+++ ws-full-stack-angular-spring-boot-tutorial/typescrypt-training/05-inheritance/Driver.ts
@@ -1,8 +1,14 @@
 import { Circle } from "./Circle";
+import { Rectangle } from "./Rectangle";
 import { Shape } from "./Shape";
 
-let myShape = new Shape(10, 15);
-console.log(myShape.getInfo());
+let myShapes: Shape[] = [];
 
 let myCircle = new Circle(5, 10, 20);
-console.log(myCircle.getInfo());
+let myRectangle = new Rectangle(0, 0, 3, 7);
+myShapes.push(myCircle);
+myShapes.push(myRectangle);
+
+myShapes.forEach(myShape => {
+    console.log(myShape.getInfo());
+});
