04d0cb7ac9c7ecf136e947ddb7a367a13cd920a3
jonataslaet <jonataslaetprogramador@gmail.com>
2023-02-02 22:27:42 -0300

    full-stack-angular-spring-boot-tutorial
    Abstract Classes

---
+++ ws-full-stack-angular-spring-boot-tutorial/typescrypt-training/06-abstract-methods/Driver.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Circle_1 = require("./Circle");
+var Rectangle_1 = require("./Rectangle");
+var myShapes = [];
+var myCircle = new Circle_1.Circle(5, 10, 20);
+var myRectangle = new Rectangle_1.Rectangle(0, 0, 3, 7);
+myShapes.push(myCircle);
+myShapes.push(myRectangle);
+myShapes.forEach(function (myShape) {
+    console.log(myShape.getInfo() + ", area = " + myShape.calculateArea());
+});
