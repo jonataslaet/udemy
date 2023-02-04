bb25b661b36b48afcb739df7980802bd477ee6a4
jonataslaet <jonataslaetprogramador@gmail.com>
2023-02-04 15:00:47 -0300

    full-stack-angular-spring-boot-tutorial
    Creating a New Angular Component
    
    Create a new project
    Update main template page
    Generate a new Component
    Add new component selector to app template page
    Generate a SalesPerson class
    In SalesPersonListComponent, create sample data
    In sales-person-list template file, build HTML table by looping over data

---
+++ ws-full-stack-angular-spring-boot-tutorial/angular-training/sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
+
+@Component({
+  selector: 'app-sales-person-list',
+  templateUrl: './sales-person-list.component.html',
+  styleUrls: ['./sales-person-list.component.css']
+})
+export class SalesPersonListComponent implements OnInit {
+
+  salesPersonList: SalesPerson[] = [
+    new SalesPerson("Jonatas", "Laet", "jonataslaet@gmail.com", 72780),
+    new SalesPerson("Dérick", "Wenner", "derickwenner@gmail.com", 72781),
+    new SalesPerson("Marcos", "Aurélio", "marcosaurelio@gmail.com", 72782),
+    new SalesPerson("Diego", "Rozado", "diegorozado@gmail.com", 72783)
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
