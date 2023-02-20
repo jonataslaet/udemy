6db37334d33624c02242e2f1fe1bfbd510b44b6e
jonataslaet <jonataslaetprogramador@gmail.com>
2023-02-19 22:21:22 -0300

    curso-flutter
    Aplicando Estilo ao Widget Questão
    Desafio Novo Widget
    Passando Função Callback

--- ws-curso-flutter/projeto_perguntas/lib/main.dart
+++ ws-curso-flutter/projeto_perguntas/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:projeto_perguntas/resposta.dart';
 
 import './questao.dart';
 
@@ -30,9 +31,9 @@ class _PerguntaAppState extends State<PerguntaApp> {
         body: Column(
           children: [
             Questao(perguntas[_perguntaSelecionada]),
-            ElevatedButton(onPressed: _responder, child: Text('Resposta 1')),
-            ElevatedButton(onPressed: _responder, child: Text('Resposta 2')),
-            ElevatedButton(onPressed: _responder, child: Text('Resposta 3')),
+            Resposta('Resposta 1', _responder),
+            Resposta('Resposta 2', _responder),
+            Resposta('Resposta 3', _responder),
           ],
         ),
       ),
