dded288015a40a252872dd0368fe4233543ee4c2
jonataslaet <jonataslaetprogramador@gmail.com>
2023-03-01 19:23:04 -0300

    restful-apis-do-0-a-nuvem-com-springboot-e-docker
    Criando o Nosso Projeto

---
+++ ws-restful-apis-do-0-a-nuvem-com-springboot-e-docker/05_FirstStepsInJavawithSpringBoot/rest-with-spring-boot-and-java-erudio/src/test/java/com/github/jonataslaet/StartupTests.java
@@ -0,0 +1,13 @@
+package com.github.jonataslaet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
