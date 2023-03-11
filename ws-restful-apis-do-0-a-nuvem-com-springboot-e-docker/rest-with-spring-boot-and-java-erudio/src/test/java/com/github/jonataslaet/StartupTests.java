93430c8270dd1e1f7262d1bebc99b92094798f18
jonataslaet <jonataslaetprogramador@gmail.com>
2023-03-11 11:53:59 -0300

    restful-apis-do-0-a-nuvem-com-springboot-e-docker: Ajustes extras
    Definindo porta no StartupTests, e fazendo-o estender AbstractIntegrationTest

--- ws-restful-apis-do-0-a-nuvem-com-springboot-e-docker/rest-with-spring-boot-and-java-erudio/src/test/java/com/github/jonataslaet/StartupTests.java
+++ ws-restful-apis-do-0-a-nuvem-com-springboot-e-docker/rest-with-spring-boot-and-java-erudio/src/test/java/com/github/jonataslaet/StartupTests.java
@@ -1,10 +1,11 @@
 package com.github.jonataslaet;
 
+import com.github.jonataslaet.integrationtests.testcontainers.AbstractIntegrationTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class StartupTests {
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+class StartupTests extends AbstractIntegrationTest {
 
 	@Test
 	void contextLoads() {
