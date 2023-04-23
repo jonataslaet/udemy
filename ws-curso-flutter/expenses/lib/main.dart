302f00bb14a505df67044d8b7fe5c2edce314cac
jonataslaet <jonataslaetprogramador@gmail.com>
2023-04-23 19:59:04 -0300

    curso-flutter: Construindo App Real - Despesas Pessoais
    Criando Classe de Modelo
    Mapear Dados para Widgets
    Item Lista Personailzado
    Aplicando Estilo no Container
    Aplicando Estilo no Texto
    Desafio Estilo Título & Data

--- ws-curso-flutter/expenses/lib/main.dart
+++ ws-curso-flutter/expenses/lib/main.dart
@@ -1,5 +1,6 @@
 
 
+import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
@@ -18,16 +19,77 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
+  final _transactions = [
+    Transaction(id: 't1', title: 'Conta de água', value: 50.76, date: DateTime.now()),
+    Transaction(id: 't2', title: 'Conta de luz', value: 211.76, date: DateTime.now())
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Despesas pessoais'),
       ),
-      body: Center(
-        child: Text('Versão 1.0.0'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              child: Text('Gráfico'),
+              elevation: 5,
+            ),
+          ),
+          Column(
+            children: _transactions.map((t){
+              return Card(
+                child: Row(
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.symmetric(
+                        horizontal: 15,
+                        vertical: 10
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.purple,
+                          width: 2,
+                        )
+                      ),
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        t.value.toString(),
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple
+                        ),
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          t.title,
+                          style: const TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          t.date.toString(),
+                          style: const TextStyle(
+                            color: Colors.grey,
+                          )
+                        ),
+                      ],
+                    )
+                 ]),
+              );
+            }).toList(),
+          )
+        ],
       ),
     );
   }
