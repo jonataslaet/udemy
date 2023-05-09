14e21e72e7374d2645e6adbf0336f4c5f6850bfd
jonataslaet <jonataslaetprogramador@gmail.com>
2023-05-09 11:30:52 -0300

    curso-flutter: Construindo App Real - Despesas Pessoais
    Usando TextField

--- ws-curso-flutter/expenses/lib/main.dart
+++ ws-curso-flutter/expenses/lib/main.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -20,75 +18,108 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  
+  final titleController = TextEditingController();
+  final valueController = TextEditingController();
+
   final _transactions = [
-    Transaction(id: 't1', title: 'Conta de água', value: 50.76, date: DateTime.now()),
-    Transaction(id: 't2', title: 'Conta de luz', value: 211.70, date: DateTime.now())
+    Transaction(
+        id: 't1', title: 'Conta de água', value: 50.76, date: DateTime.now()),
+    Transaction(
+        id: 't2', title: 'Conta de luz', value: 211.70, date: DateTime.now())
   ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Despesas pessoais'),
+        title: const Text('Despesas pessoais'),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Container(
+          const SizedBox(
             width: double.infinity,
-            child: Card(
-              color: Colors.blue,
-              child: Text('Gráfico'),
-              elevation: 5,
-            ),
+            child:
+                Card(color: Colors.blue, elevation: 5, child: Text('Gráfico')),
           ),
           Column(
-            children: _transactions.map((t){
+            children: _transactions.map((t) {
               return Card(
-                child: Row(
-                  children: [
-                    Container(
-                      margin: const EdgeInsets.symmetric(
-                        horizontal: 15,
-                        vertical: 10
-                      ),
-                      decoration: BoxDecoration(
+                child: Row(children: [
+                  Container(
+                    margin: const EdgeInsets.symmetric(
+                        horizontal: 15, vertical: 10),
+                    decoration: BoxDecoration(
                         border: Border.all(
-                          color: Colors.purple,
-                          width: 2,
-                        )
-                      ),
-                      padding: const EdgeInsets.all(10),
-                      child: Text(
-                        'R\$ ${t.value.toStringAsFixed(2)}',
-                        style: const TextStyle(
+                      color: Colors.purple,
+                      width: 2,
+                    )),
+                    padding: const EdgeInsets.all(10),
+                    child: Text(
+                      'R\$ ${t.value.toStringAsFixed(2)}',
+                      style: const TextStyle(
                           fontWeight: FontWeight.bold,
                           fontSize: 20,
-                          color: Colors.purple
+                          color: Colors.purple),
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        t.title,
+                        style: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          t.title,
-                          style: const TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        Text(
-                          DateFormat('d MMM y').format(t.date),
+                      Text(DateFormat('d MMM y').format(t.date),
                           style: const TextStyle(
                             color: Colors.grey,
-                          )
-                        ),
-                      ],
-                    )
-                 ]),
+                          )),
+                    ],
+                  )
+                ]),
               );
             }).toList(),
+          ),
+          Card(
+            elevation: 5,
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                children: [
+                  TextField(
+                    controller: titleController,
+                    decoration: const InputDecoration(
+                      labelText: 'Título',
+                    ),
+                  ),
+                  TextField(
+                    controller: valueController,
+                    decoration: const InputDecoration(
+                      labelText: 'Valor (R\$)',
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      TextButton(
+                        onPressed: () {
+                          print(titleController.text);
+                          print(valueController.text);
+                        },
+                        style: TextButton.styleFrom(
+                          backgroundColor: Colors.white,
+                          foregroundColor: Colors.purple,
+                        ),
+                        child: const Text("Nova Transação"),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
           )
         ],
       ),
