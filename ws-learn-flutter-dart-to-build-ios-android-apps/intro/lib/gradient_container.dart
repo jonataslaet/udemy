2478bc534c449baa9a6cb7f83c6b52bb9c35cc6b
jonataslaet <jonataslaetprogramador@gmail.com>
2023-05-28 22:20:01 -0300

    Section 2: Flutter & Dart Basics I - Getting a Solid Foundation [ROLL DICE APP]
    Practise: Reusable Widgets & Constructor Functions

---
+++ ws-learn-flutter-dart-to-build-ios-android-apps/intro/lib/gradient_container.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:intro/styled_text.dart';
+
+class GradiantContainer extends StatelessWidget {
+  const GradiantContainer(this.color1, this.color2, {super.key});
+  final Color color1;
+  final Color color2;
+  
+  @override
+  Widget build(context) {
+    return Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                color1,
+                color2,
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const Center(
+            child: StyledText('Hello, World.'),
+          ),
+        );
+  }
+}
--- ws-learn-flutter-dart-to-build-ios-android-apps/intro/lib/main.dart
+++ ws-learn-flutter-dart-to-build-ios-android-apps/intro/lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:intro/gradient_container.dart';
 
 void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
-        backgroundColor: Color.fromARGB(255, 65, 73, 196),
-        body: Center(
-          child: Text('Hello, World'),
+        body: GradiantContainer(
+          Color.fromARGB(255, 26, 2, 80),
+          Color.fromARGB(255, 45, 7, 98),
         ),
       ),
     ),
