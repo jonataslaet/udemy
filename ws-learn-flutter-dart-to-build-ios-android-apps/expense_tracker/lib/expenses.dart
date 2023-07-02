ced6da7df2f6322c7a417f3239e2e2676f68b20f
jonataslaet <jonataslaetprogramador@gmail.com>
2023-07-02 00:12:30 -0300

    Section 5: Adding Interactivity, More Widgets & Theming [EXPENSE TRACKER APP]
    Creating Dummy Data
    Efficiently Rendering Long List with ListView
    Using Lists Inside Of Lists
    Creating a Custom List Item with Card & Spacer Widgets

--- ws-learn-flutter-dart-to-build-ios-android-apps/expense_tracker/lib/expenses.dart
+++ ws-learn-flutter-dart-to-build-ios-android-apps/expense_tracker/lib/expenses.dart
@@ -1,3 +1,5 @@
+import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -9,15 +11,22 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesStates extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+        title: 'Flutter Course',
+        amount: 19.99,
+        date: DateTime.now(),
+        category: Category.work),
+    Expense(
+        title: 'Cinema',
+        amount: 15.79,
+        date: DateTime.now(),
+        category: Category.leisure),
+  ];
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(
-        children: [
-          Text('Teh chart'),
-          Text('Expenses List'),
-        ],
-      ),
+    return Scaffold(
+      body: ExpensesList(expenses: _registeredExpenses),
     );
   }
 }
