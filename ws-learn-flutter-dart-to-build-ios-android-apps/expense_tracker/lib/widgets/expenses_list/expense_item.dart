f1e0c59ae6d11e5f372758b68b1b28e1108d7ccc
jonataslaet <jonataslaetprogramador@gmail.com>
2023-07-03 08:41:17 -0300

    Section 5: Adding Interactivity, More Widgets & Theming [EXPENSE TRACKER APP]
    Using Icons & Formatting

--- ws-learn-flutter-dart-to-build-ios-android-apps/expense_tracker/lib/widgets/expenses_list/expense_item.dart
+++ ws-learn-flutter-dart-to-build-ios-android-apps/expense_tracker/lib/widgets/expenses_list/expense_item.dart
@@ -8,23 +8,22 @@ class ExpenseItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Card(
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
         child: Column(
           children: [
             Text(expense.title),
-            SizedBox(height: 4,),
+            const SizedBox(height: 4,),
             Row(
               children: [
                 Text('\$${expense.amount.toStringAsFixed(2)}'),
                 const Spacer(),
                 Row(
                   children: [
-                    const Icon(Icons.alarm),
+                    Icon(categoryIcons[expense.category]),
                     const SizedBox(width: 8,),
-                    Text(expense.date.toString()),
+                    Text(expense.formattedDate),
                   ],
                 ),
               ],
