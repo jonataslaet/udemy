0f0faabe4d9374cdcb520ca101c1d3fbf29f52bd
jonataslaet <jonataslaetprogramador@gmail.com>
2023-07-10 08:52:06 -0300

    Section 5: Adding Interactivity, More Widgets & Theming [EXPENSE TRACKER APP]
    Showind a Date Picker

--- ws-learn-flutter-dart-to-build-ios-android-apps/expense_tracker/lib/widgets/expenses_list/new_expense.dart
+++ ws-learn-flutter-dart-to-build-ios-android-apps/expense_tracker/lib/widgets/expenses_list/new_expense.dart
@@ -13,6 +13,17 @@ class _NewExpenseState extends State<NewExpense> {
   final titleController = TextEditingController();
   final amountController = TextEditingController();
 
+  void _presentDatePicker() {
+    final now = DateTime.now();
+    final firstDate = DateTime(now.year - 1, now.month, now.day);
+    showDatePicker(
+      context: context,
+      initialDate: now,
+      firstDate: firstDate,
+      lastDate: now,
+    );
+  }
+
   @override
   void dispose() {
     super.dispose();
@@ -35,16 +46,38 @@ class _NewExpenseState extends State<NewExpense> {
               ),
             ),
           ),
-          TextField(
-            controller: amountController,
-            keyboardType: TextInputType.number,
-            maxLength: 50,
-            decoration: const InputDecoration(
-              prefixText: '\$ ',
-              label: Text(
-                'Amount',
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: amountController,
+                  keyboardType: TextInputType.number,
+                  maxLength: 50,
+                  decoration: const InputDecoration(
+                    prefixText: '\$ ',
+                    label: Text(
+                      'Amount',
+                    ),
+                  ),
+                ),
               ),
-            ),
+              const SizedBox(
+                width: 16,
+              ),
+              Expanded(
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    const Text('Select a date'),
+                    IconButton(
+                      onPressed: _presentDatePicker,
+                      icon: const Icon(Icons.calendar_month),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
           Row(
             children: [
