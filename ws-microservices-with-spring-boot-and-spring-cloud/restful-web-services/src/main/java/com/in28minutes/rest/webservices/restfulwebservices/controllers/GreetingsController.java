ad76a28bd2ef1c8c4713e4f65adf7e586be49cf4
jonataslaet <jonataslaetprogramador@gmail.com>
2023-07-18 11:39:30 -0300

    Section 03: Restful Web Services with Spring Boot - V2
    Step 18 - Exploring Content Negotiation - Implementing Support for XML
    Step 19 - Exploring Internationalization for REST API

--- ws-microservices-with-spring-boot-and-spring-cloud/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/controllers/GreetingsController.java
+++ ws-microservices-with-spring-boot-and-spring-cloud/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/controllers/GreetingsController.java
@@ -1,12 +1,22 @@
 package com.in28minutes.rest.webservices.restfulwebservices.controllers;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 public class GreetingsController {
+
+    private final MessageSource messageSource;
+
+    public GreetingsController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
 
     @GetMapping("/hello")
     public ResponseEntity<?> getHello() {
@@ -23,4 +33,11 @@ public class GreetingsController {
     public GreatingBean getHelloBeanByName(@PathVariable("name") String name) {
         return new GreatingBean(String.format("Hello, %s", name));
     }
+
+    @GetMapping("/hello-internationalized")
+    public ResponseEntity<?> getHelloInternationalized() {
+        Locale locale = LocaleContextHolder.getLocale();
+        return ResponseEntity.ok().body(messageSource.getMessage("greeting.message", null, "Default Message", locale));
+    }
+
 }
