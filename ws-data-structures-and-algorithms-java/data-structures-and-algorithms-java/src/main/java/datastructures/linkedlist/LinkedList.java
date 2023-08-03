336bdb194a58bf870084af938f0e33bdbc5d2ba9
jonataslaet <jonataslaetprogramador@gmail.com>
2023-08-02 23:29:34 -0300

    Section 04: Linked Lists
    LL: Constructor

---
+++ ws-data-structures-and-algorithms-java/data-structures-and-algorithms-java/src/main/java/datastructures/linkedlist/LinkedList.java
@@ -0,0 +1,14 @@
+package datastructures.linkedlist;
+
+public class LinkedList {
+    private Node head;
+    private Node tail;
+    private int length;
+
+    public LinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+}
