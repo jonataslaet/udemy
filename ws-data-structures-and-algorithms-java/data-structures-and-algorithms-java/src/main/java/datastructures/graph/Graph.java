a09b63c2307d117689a8e3204ac13f3e57b99774
jonataslaet <jonataslaetprogramador@gmail.com>
2023-09-17 22:06:20 -0300

    Section 18: Graphs
    Graph: Add Vertex

---
+++ ws-data-structures-and-algorithms-java/data-structures-and-algorithms-java/src/main/java/datastructures/graph/Graph.java
@@ -0,0 +1,19 @@
+package datastructures.graph;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Graph {
+    private HashMap<String, ArrayList<String>> adjList = new HashMap<>();
+
+    public boolean addVertex(String vertex) {
+        if (adjList.get(vertex) != null) return false;
+        adjList.put(vertex, new ArrayList<String>());
+        return true;
+    }
+
+    public void printGraph() {
+        System.out.println(adjList);
+    }
+
+}
---
+++ ws-data-structures-and-algorithms-java/data-structures-and-algorithms-java/src/main/java/datastructures/graph/Main.java
@@ -0,0 +1,10 @@
+package datastructures.graph;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Graph graph = new Graph();
+        graph.addVertex("A");
+        graph.printGraph();
+    }
+}
