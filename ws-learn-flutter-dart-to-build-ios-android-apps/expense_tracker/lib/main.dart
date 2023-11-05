d1e10b9ceec15c5a6e8d68e2e6369d280c425e8f
jonataslaet <jonataslaetprogramador@gmail.com>
2023-11-05 13:14:12 -0300

    Section 6: Building Responsive & Adaptive User Interfaces [EXPENSE TRACKER APP]
    Locking the Device Orientiation - Unlocking

--- ws-learn-flutter-dart-to-build-ios-android-apps/expense_tracker/lib/main.dart
+++ ws-learn-flutter-dart-to-build-ios-android-apps/expense_tracker/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:expense_tracker/expenses.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 var kColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 96, 59, 181),
@@ -12,10 +11,10 @@ var kDarkColorScheme = ColorScheme.fromSeed(
 );
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp,
-  ]).then((fn) {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // SystemChrome.setPreferredOrientations([
+  //   DeviceOrientation.portraitUp,
+  // ]).then((fn) {
     runApp(MaterialApp(
       darkTheme: ThemeData.dark().copyWith(
         useMaterial3: true,
@@ -62,5 +61,5 @@ void main() {
       ),
       home: const Expenses(),
     ));
-  });
+  // });
 }
