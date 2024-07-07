f8ba9a9b5057f7def2eddbe99c18406d8e513cee
Jonatas Laet <jonataslaetprogramador@gmail.com>
2024-07-07 15:33:51 -0300

    Section 08 - Created remove and get methods, and unit tests

--- ws-java-data-structures-and-algorithms-masterclass/java-data-structures-and-algorithms-masterclass/src/main/java/section08/Main.java
+++ ws-java-data-structures-and-algorithms-masterclass/java-data-structures-and-algorithms-masterclass/src/main/java/section08/Main.java
@@ -5,8 +5,12 @@ public class Main {
     public static void main(String[] args) {
         SinglyLinkedList list = new SinglyLinkedList();
         list.insert(0, 10);
-        list.insert(1, 20);
-        list.insert(2, 30);
+        list.insert(0, 20);
+        list.insert(0, 30);
+        Node removedNode = null;
+//        removedNode = list.pop();
+        removedNode = list.remove(1);
+        System.out.println("Removed node value = " + removedNode.getValue());
         list.showAll();
     }
 }
--- ws-java-data-structures-and-algorithms-masterclass/java-data-structures-and-algorithms-masterclass/src/test/java/section08/SinglyLinkedListTests.java
+++ ws-java-data-structures-and-algorithms-masterclass/java-data-structures-and-algorithms-masterclass/src/test/java/section08/SinglyLinkedListTests.java
@@ -12,6 +12,63 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class SinglyLinkedListTests {
 
+    @Test
+    void testRemoveElementAtTheMiddleOfANonEmptyList() {
+        SinglyLinkedList list = new SinglyLinkedList();
+        list.insert(0, 10);
+        list.insert(0, 20);
+        list.insert(0, 30);
+        int expectedRemovedNodeValue = 20;
+        int removedNodeValue = list.remove(1).getValue();
+        String resultedList = getOutputFromList(list);
+        String expectedList = "30 -> 10";
+
+        assertEquals(expectedRemovedNodeValue, removedNodeValue);
+        assertEquals(expectedList, resultedList);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 3, 4, 5, 6, 7, 8})
+    void testRemoveElementAtInvalidPositionOfListOfSize2(int invalidPosition) {
+        SinglyLinkedList list = new SinglyLinkedList();
+        list.insert(0, 10);
+        list.insert(0, 20);
+
+        IndexOutOfBoundsException exception = assertThrows(IndexOutOfBoundsException.class, () -> {
+            list.remove(invalidPosition);
+        });
+        String expectedMessage = "Invalid position for this value to be read";
+        String actualMessage = exception.getLocalizedMessage();
+        assertEquals(expectedMessage, actualMessage);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 1})
+    void testRemoveElementAtInvalidPositionOfEmptyList(int invalidPosition) {
+        SinglyLinkedList list = new SinglyLinkedList();
+        IndexOutOfBoundsException exception = assertThrows(IndexOutOfBoundsException.class, () -> {
+            list.remove(invalidPosition);
+        });
+        String expectedMessage = "Invalid position for this value to be read";
+        String actualMessage = exception.getLocalizedMessage();
+        assertEquals(expectedMessage, actualMessage);
+    }
+
+    @Test
+    void testRemoveElementAtTheEndOfANonEmptyList() {
+        SinglyLinkedList list = new SinglyLinkedList();
+        list.insert(0, 10);
+        list.insert(0, 20);
+        list.insert(0, 30);
+        int expectedNodeValue = 10;
+        int removedNodeValue = list.pop().getValue();
+        String resultedList = getOutputFromList(list);
+        String expectedList = "30 -> 20";
+
+        assertEquals(expectedNodeValue, removedNodeValue);
+        assertEquals(expectedList, resultedList);
+    }
+
     @ParameterizedTest
     @ValueSource(ints = {-1, 1})
     void testInsertElementAtInvalidPositionOfEmptyList(int invalidPosition) {
