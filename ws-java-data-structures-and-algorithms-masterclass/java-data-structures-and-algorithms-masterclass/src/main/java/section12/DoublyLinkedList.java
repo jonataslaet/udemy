a7a3cad1b1d78412f7a1199a8277288290f5b231
Jonatas Laet <jonataslaetprogramador@gmail.com>
2024-07-30 21:14:54 -0300

    Section 12 - Create method to insert at the beginning, and tests

--- ws-java-data-structures-and-algorithms-masterclass/java-data-structures-and-algorithms-masterclass/src/main/java/section12/DoublyLinkedList.java
+++ ws-java-data-structures-and-algorithms-masterclass/java-data-structures-and-algorithms-masterclass/src/main/java/section12/DoublyLinkedList.java
@@ -1,5 +1,7 @@
 package section12;
 
+import section08.Node;
+
 public class DoublyLinkedList {
     private DoublyNode head;
     private DoublyNode tail;
@@ -12,6 +14,23 @@ public class DoublyLinkedList {
         this.createFirstNode(value);
     }
 
+    private void insertAtTheBeginning(int value) {
+        DoublyNode node = new DoublyNode(value);
+        node.setNext(head);
+        head.setPrevious(node);
+        head = node;
+        this.size++;
+    }
+
+    public void insert(int i, int value) {
+        validPositionToInsert(i);
+        if (isEmptyList()) {
+            createFirstNode(value);
+        } else if (i == 0) {
+            insertAtTheBeginning(value);
+        }
+    }
+
     private void createFirstNode(int value) {
         DoublyNode node = new DoublyNode(value);
         node.setNext(null);
@@ -21,7 +40,23 @@ public class DoublyLinkedList {
         this.size = 1;
     }
 
-    public void showAll() {
+    private void validPositionToInsert(int i) {
+        if (i > this.size || i < 0) {
+            throw new IndexOutOfBoundsException("Invalid position for this value to be inserted");
+        }
+    }
+
+    private void validPositionToRead(int i) {
+        if (i >= this.size || i < 0) {
+            throw new IndexOutOfBoundsException("Invalid position for this value to be read");
+        }
+    }
+
+    private boolean isEmptyList() {
+        return size == 0;
+    }
+
+    public void showAllFromHeadToTail() {
         DoublyNode temp = head;
         if (temp == null) {
             System.out.println("There's no element in this list");
@@ -32,6 +67,20 @@ public class DoublyLinkedList {
         while (temp != null) {
             System.out.print(" -> " + temp.getValue());
             temp = temp.getNext();
+        }
+    }
+
+    public void showAllFromTailToHead() {
+        DoublyNode temp = tail;
+        if (temp == null) {
+            System.out.println("There's no element in this list");
+        } else {
+            System.out.print(temp.getValue());
+            temp = temp.getPrevious();
+        }
+        while (temp != null) {
+            System.out.print(" -> " + temp.getValue());
+            temp = temp.getPrevious();
         }
     }
 }
--- ws-java-data-structures-and-algorithms-masterclass/java-data-structures-and-algorithms-masterclass/src/test/java/section12/DoublyLinkedListTests.java
+++ ws-java-data-structures-and-algorithms-masterclass/java-data-structures-and-algorithms-masterclass/src/test/java/section12/DoublyLinkedListTests.java
@@ -1,5 +1,6 @@
 package section12;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -10,20 +11,45 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DoublyLinkedListTests {
 
+    @Test
+    void testCreateDoublyLinkedListWithFirstNode() {
+        DoublyLinkedList list = new DoublyLinkedList();
+        list.insert(0, 12);
+        list.insert(0, 11);
+        list.insert(0, 10);
+        String actualOutput = getOutputFromListFromHeadToTail(list);
+        String expectedOutput = "10 -> 11 -> 12";
+        assertEquals(expectedOutput, actualOutput);
+
+        actualOutput = getOutputFromListFromTailToHead(list);
+        expectedOutput = "12 -> 11 -> 10";
+        assertEquals(expectedOutput, actualOutput);
+    }
+
     @ParameterizedTest
     @ValueSource(ints = {-199, 323, 445, 512, 632, 733, 855})
     void testCreateDoublyLinkedListWithFirstNode(int value) {
         DoublyLinkedList list = new DoublyLinkedList(value);
-        String actualOutput = getOutputFromList(list);
+        String actualOutput = getOutputFromListFromHeadToTail(list);
         String expectedOutput = "" + value;
         assertEquals(expectedOutput, actualOutput);
+        assertEquals(expectedOutput, getOutputFromListFromTailToHead(list));
     }
 
-    private String getOutputFromList(DoublyLinkedList list) {
+    private String getOutputFromListFromHeadToTail(DoublyLinkedList list) {
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         PrintStream originalOut = System.out;
         System.setOut(new PrintStream(outputStream));
-        list.showAll();
+        list.showAllFromHeadToTail();
+        System.setOut(originalOut);
+        return outputStream.toString().trim();
+    }
+
+    private String getOutputFromListFromTailToHead(DoublyLinkedList list) {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream originalOut = System.out;
+        System.setOut(new PrintStream(outputStream));
+        list.showAllFromTailToHead();
         System.setOut(originalOut);
         return outputStream.toString().trim();
     }
